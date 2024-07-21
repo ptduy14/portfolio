b335fd1817fda4c5b32c6648ff36e7cd94c19c4d
ptduy14 <phantanduy14@gmail.com>
2024-07-21 22:47:18 +0700

    feat: complete responsive for mobile and tablet devices

--- src/components/Content/SubContent/Skill.jsx
+++ src/components/Content/SubContent/Skill.jsx
@@ -3,7 +3,7 @@ export default function Skill() {
     <div className="flex flex-col gap-y-6 overflow-y-scroll h-full w-full">
       <div className="">
         <h3 className="text-lg mb-3 font-semibold">👨‍💻 Front-End</h3>
-        <div className="flex justify-start">
+        <div className="flex justify-start flex-wrap">
           <img alt="" src="https://img.shields.io/badge/next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white" />
           <img alt="" src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" />
           <img alt="" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
@@ -15,21 +15,21 @@ export default function Skill() {
       </div>
       <div className="">
         <h3 className="text-lg mb-3 font-semibold">⚙️ Back-End</h3>
-        <div className="flex justify-start">
+        <div className="flex justify-start flex-wrap">
           <img alt="" src="https://img.shields.io/badge/Laravel-FF2D20?style=for-the-badge&logo=laravel&logoColor=white" />
           <img alt="" src="https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white" />
         </div>
       </div>
       <div className="">
         <h3 className="text-lg mb-3 font-semibold">⚡ Database</h3>
-        <div className="flex justify-start">
+        <div className="flex justify-start flex-wrap">
           <img alt="" src="https://img.shields.io/badge/Microsoft%20SQL%20Server-CC2927?style=for-the-badge&logo=microsoft%20sql%20server&logoColor=white" />
           <img alt="" src="https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white" />
         </div>
       </div>
       <div className="">
         <h3 className="text-lg mb-3 font-semibold">☁ Cloud</h3>
-        <div className="flex justify-start">
+        <div className="flex justify-start flex-wrap">
         <img alt="" src="https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white" />
           <img alt="" src="https://img.shields.io/badge/Cloudinary-3448C5?style=for-the-badge&logo=Cloudinary&logoColor=white" />
           <img alt="" src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" />
@@ -37,7 +37,7 @@ export default function Skill() {
       </div>
       <div className="">
         <h3 className="text-lg mb-3 font-semibold">💻 OS</h3>
-        <div className="flex justify-start">
+        <div className="flex justify-start flex-wrap">
         <img alt="" src="https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white" />
         <img alt="" src="https://img.shields.io/badge/Windows-0078D6?style=for-the-badge&logo=windows&logoColor=white" />
         </div>
