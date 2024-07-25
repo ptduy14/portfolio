07ca053cb59e0b97471d1049e6a2600a67eb00d7
ptduy14 <phantanduy14@gmail.com>
2024-07-25 20:23:48 +0700

    Revert "commit"
    This reverts commit 5749d84223c10ecf3ea97b5d777683170c11568b.

--- src/components/Content/SubContent/Chatbot.jsx
+++ src/components/Content/SubContent/Chatbot.jsx
@@ -2,9 +2,10 @@ import { Standard } from "@typebot.io/react";
 
 export default function Chatbot() {
   return (
-    <Standard
-      typebot="porfolio-bot-evvxj5y"
-      style={{ width: "100%", height: "" }}
-    />
+      <Standard
+        
+        typebot="porfolio-bot-evvxj5y"
+        style={{ width: "100%", height: "" }}
+      />
   );
 }
