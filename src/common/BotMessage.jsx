5d183959f12c456134688007ba5a6748c8337689
ptduy14 <phantanduy14@gmail.com>
2024-07-28 16:59:34 +0700

    style: change text align message

--- src/common/BotMessage.jsx
+++ src/common/BotMessage.jsx
@@ -7,7 +7,7 @@ export default function BotMessage({ message }) {
       <div className="hidden md:block md:h-12 md:w-12 rounded-full overflow-hidden flex-shrink-0">
         <img className="h-full w-full" src={avata} alt="" />
       </div>
-      <div className="bg-tertiary-color px-2 py-2 md:px-6 md:py-3 rounded-md text-justify">
+      <div className="bg-tertiary-color px-2 py-2 md:px-6 md:py-3 rounded-md">
         {message}
       </div>
     </div>
