2143604ada3ccf9ac7efd96b096fc49f0aa9b3a9
ptduy14 <phantanduy14@gmail.com>
2024-07-28 13:11:33 +0700

    refactor: logic render message

--- src/common/UserMessage.jsx
+++ src/common/UserMessage.jsx
@@ -1,7 +1,7 @@
 export default function UserMessage({ message }) {
   return (
     <div className="text-end">
-      <div className="bg-[#ddba20] text-white md:text-text-color md:font-normal text-left font-semibold md:bg-tertiary-color px-2 py-2 md:px-6 md:py-3 inline-block rounded-md">
+      <div className="bg-[#ddba20] text-white md:text-text-color md:font-normal text-left font-semibold md:bg-hover-color px-2 py-2 md:px-6 md:py-3 inline-block rounded-md">
         {message}
       </div>
     </div>
