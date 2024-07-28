6f2b4a8119edd1352715562e93f5ed298a5d4a85
ptduy14 <phantanduy14@gmail.com>
2024-07-28 20:33:04 +0700

    style: update avata

--- src/components/Header/Avata.jsx
+++ src/components/Header/Avata.jsx
@@ -1,8 +1,10 @@
+import avata from "../../assets/img/avata.jpg"
+
 export default function Avata() {
   return (
     <div className="w-40 h-40 rounded-full overflow-hidden inline-block">
       <img
-        src="https://upload.wikimedia.org/wikipedia/en/thumb/3/30/Jon_Snow_Season_8.png/220px-Jon_Snow_Season_8.png"
+        src={avata}
         alt="avata"
       />
     </div>
